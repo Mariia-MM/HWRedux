1b90bc045311e316bacfca5dc5b19d776a316811
Mariia <mytsko.mariia@gmail.com>
2020-09-28 19:31:55 +0200

    Updates

---
+++ precache-manifest.c578fba9fffbd4e8f6df7b48a7423c01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95121c16b97860b8891e820ef00155a9",
+    "url": "/HWRedux/index.html"
+  },
+  {
+    "revision": "6fd3a1c008116da673f6",
+    "url": "/HWRedux/static/css/main.a6e8a759.chunk.css"
+  },
+  {
+    "revision": "9b953ca2d043ce1a6df4",
+    "url": "/HWRedux/static/js/2.cbf690f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HWRedux/static/js/2.cbf690f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fd3a1c008116da673f6",
+    "url": "/HWRedux/static/js/main.0d8ad12d.chunk.js"
+  },
+  {
+    "revision": "8ae68435c209b47d3114",
+    "url": "/HWRedux/static/js/runtime-main.c0a4a833.js"
+  }
+]);
